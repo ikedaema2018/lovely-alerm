7d3f84e05fe64ca06ea13067cb63d5e70b223e54
ikedaema2018 <ikedaema0817@gmail.com>
2019-03-10 00:17:57 +0900

    testでHelloコンポーネント作成　React nativeに基本を学ぶ

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './Hello'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello react native</Text>
-      </View>
-    );
-  }
+type Props = {};
+
+export default class App extends React.Component<Props> {
+    render() {
+        return (
+          <View style={styles.container}>
+              <Text>Hello react native</Text>
+              <Hello name='Expo' enthusiasmLevel={10} />
+          </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
